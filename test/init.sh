c67aa7da67a4f6328c86c8e00c41b6b2b5e57da4
Li-Shiang-Chi <lsc830621@gmail.com>
2017-05-30 20:27:14 +0800

    atca commit

--- test/init.sh
+++ test/init.sh
@@ -1,7 +1,3 @@
-
-
-
-
 sudo mmsh createcluster test_c primary 85 127.0.0.1
-sudo mmsh addnode test_c backup 192.168.1.101 86
-sudo mmsh addnode test_c slave 192.168.1.102 87
+sudo mmsh addnode test_c backup 192.168.1.27 86
+sudo mmsh addnode test_c slave 192.168.1.28 87
--- test/initAll.sh
+++ test/initAll.sh
@@ -1,9 +1,9 @@
-sudo sshpass -p 'root' ssh primary@192.168.1.100 'bash -s' < initP.sh
-sudo sshpass -p 'root' ssh backup-node@192.168.1.101 'bash -s' < initB.sh
-sudo sshpass -p 'root' ssh slave@192.168.1.102 'bash -s' < initS.sh
+sudo sshpass -p 'root' ssh primary@192.168.1.25 'bash -s' < initP.sh
+sudo sshpass -p 'root' ssh backup-node@192.168.1.27 'bash -s' < initB.sh
+sudo sshpass -p 'root' ssh slave@192.168.1.28 'bash -s' < initS.sh
 
 
 
-sudo sshpass -p 'root' ssh primary@192.168.1.100 'bash -s' < init.sh
+sudo sshpass -p 'root' ssh primary@192.168.1.25 'bash -s' < init.sh
 #sudo sshpass -p 'root' ssh backup-node@192.168.1.101 'bash -s' < mm_initial.sh
 #sudo sshpass -p 'root' ssh slave@192.168.1.102 'bash -s' < xxx_init.sh
